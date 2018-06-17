f9ad2afb27d8932c23a75fdb14d0c48b00859d8f
lucas <lucas@thestrangeagency.com>
2018-06-17 11:33:26 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Josefin', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+p {
+    font-family: 'Josefin Slab', serif;
+    font-size: 32px;
+    line-height: 48px;
+}
+</style>
