8476413ff577d04db2cabf03f26d6628b417497c
lucas <lucas@thestrangeagency.com>
2018-06-17 11:42:46 +0200

    styles and cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -16,14 +15,18 @@ export default {
   font-family: 'Josefin', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
 }
 
 p {
     font-family: 'Josefin Slab', serif;
     font-size: 32px;
     line-height: 48px;
+    padding-left: 24px;
+}
+
+a {
+    color: #08c;
+    text-decoration: none;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,30 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <p>Paragraph</a>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div>
+      <p>Lucas Kuzma is an itinerant <a title="The Strange Agency" href="http://the.strange.agency" target="_blank">application programmer</a> and <a title="Strange Software" href="http://strange.software">creative coder</a></p>
+
+      <p>with a <a href="http://lucaskuzma.github.io/docs/cv/kuzma.html">background</a> in computer science, media art, music, and audio analysis, synthesis, and visualization</p>
+
+      <p>experience in <a title="LinkedIn" href="https://www.linkedin.com/in/lucaskuzma" target="_blank">mobile, web, e-commerce, dev ops</a>, <a title="An Interface for Sequencing with Concatenative Sound Synthesis" href="http://mtg.upf.edu/node/2190" target="_blank">research</a> and <a title="USC" href="https://cinema.usc.edu/directories/profile.cfm?id=33652" target="_blank">education</a></p>
+
+      <p>the pleasure of working with agencies like Goodby, Silverstein &amp; Partners, Huge, Sapient, TBWA\Chiat\Day, and theLab</p>
+
+      <p>for clients including Absolut, Adidas, Adobe, Altoids, Balthaser Studios, Belief, Berkeley Systems, Chophouse Records, Cocoon, Covalent Networks, Discover Card, Disney, Dwell Magazine, eBay, Enlighten, Ernst &amp; Young, Hewlett Packard, House Party, IBM, Inktomi, K-Swiss, Kid Brands, Listen Labs, Lundberg Design, Macromedia, Michele Saee, Muzik, Nabisco, Nickelodeon, New Mentor, Pfizer, Philtre Com Llc, Procter &amp; Gamble, Progressive Insurance, Prudential, Redken, Revlon, Royal Elastics, Sonic Branding Solutions, Swoon Reads, Victoria&#8217;s Secret, and Yamaha</p>
+
+      <p>using Swift, Objective C, C++, Python, PHP, and JavaScript </p>
+
+      <p>on iOS, LAMP, LEMP, AWS, Heroku, Docker, Arduino, and AVR </p>
+
+      <p>with OpenGL, Three.js, React Native, Kohana, Django, and Flask</p>
+
+      <p><a title="Tweet" href="https://twitter.com/lucashenrykuzma">@lucashenrykuzma</a></p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
