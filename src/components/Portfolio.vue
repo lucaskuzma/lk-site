2f82e7b531d4f60ba574bdf5ea4bd922406fdd8c
lucas <lucas@thestrangeagency.com>
2018-07-26 13:29:37 +0200

    dynamically swap images

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -10,20 +10,21 @@
           <span class="agency title">Agency: {{ project.agency }}</span></p>
           <p class="details">{{ project.details }}</p>
 
-          <div v-if="project.images && project.images.length">
-            <div v-for="image of project.images" :key="image.id">
-              <img class="web-image" :src="'static/img/' + image" />{{image}}
-            </div>
-          </div>
+          <!--<div v-if="project.images && project.images.length">-->
+            <!--<div v-for="image of project.images" :key="image.id">-->
+              <!--<img class="web-image" :src="'static/img/' + image" />{{image}}-->
+            <!--</div>-->
+          <!--</div>-->
 
-        </div>
+          <img class="web-image" :src="'static/img/' + project.image" />{{project.image}}
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-module.exports = {
+export default {
   data: () => ({
     projects: [
       {
@@ -33,7 +34,8 @@ module.exports = {
         'images': [
           'cocoon-1.jpeg',
           'cocoon-2.jpeg'
-        ]
+        ],
+        'image': ''
       },
       {
         'name': 'K-Swiss',
@@ -41,7 +43,30 @@ module.exports = {
         'details': 'International apparel store. Magento back-end with a custom Kohana front-end. Hosted on AWS in the US and Europe. Handled all architecture, dev-ops, ERP integrations, application programming, and maintenance. Managed a team of 4 developers and a designer.'
       }
     ]
-  })
+  }),
+  created () {
+    let self = this
+
+    for (let project of self.$data.projects) {
+      if (project.images) {
+        project.image = project.images[0]
+        project.index = 0
+      }
+    }
+
+    self.interval = setInterval(function () {
+      for (let project of self.$data.projects) {
+        if (project.images) {
+          project.index = project.index === project.images.length - 1 ? 0 : project.index + 1
+          project.image = project.images[project.index]
+        }
+      }
+    }, 5000)
+  },
+  beforeDestroy () {
+    let self = this
+    clearInterval(self.interval)
+  }
 }
 </script>
 
