310802f738c873be2346bc51f062b70fd782bf93
lucas <lucas@thestrangeagency.com>
2018-07-26 11:32:55 +0200

    a little cleanup

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@ export default {
 <style>
 #app {
   font-family: 'Josefin', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   color: #000;
 }
 
